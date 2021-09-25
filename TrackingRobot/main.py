b10e250f29dda1b3cfe5f5e9a44f7b4b2d4642f6
shadihalloun35 <shadi_h7@hotmail.com>
2021-09-25 03:44:46 +0300

    video tracking

--- TrackingRobot/main.py
+++ TrackingRobot/main.py
@@ -5,12 +5,15 @@ Created on Wed Sep 22 19:41:51 2021
 @author: shadi
 """
 import VideoHandling
+import ObjectTracking
 
 
-videoPath = 'C:\\HaifaUniversity\\RBD Lab\\RBD-Lab-AI634-Task\\ai634Data\\wetransfer_algo_2021-08-26_1138\\Camera1_1280_720.mp4'
+videoPath = 'C:\\HaifaUniversity\\RBD Lab\\RBD-Lab-AI634-Task\\ai634Data\\wetransfer_algo_2021-08-26_1138\\Camera2_1280_720.mp4'
 
 if __name__=="__main__":
-    VideoHandling.DisplayVideo(videoPath)
+    
+    #VideoHandling.DisplayVideo(videoPath)
     #VideoHandling.WritingVideo()
+    ObjectTracking.TrackObject(videoPath)
     
     
